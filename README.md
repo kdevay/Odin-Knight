2af03d9470405bc901e65f0bbc2538dadb3f8580
Katherine de Vay <katdevay2@gmail.com>
2023-01-10 21:06:27 -0600

    Finish refactoring

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-Now you’re a pro with DFS and BFS. Let’s try using our search algorithms on a real problem.
-
-For this project, you’ll need to use a data structure that’s similar (but not identical) to a binary tree. For a summary of a few different examples, reference this article.
-
 Given that a knight can two steps forward and one step to the side in any direction:
 build a function (knightMoves) that shows the shortest route from one square to another 
 outputting all squares the knight will stop on along the way.
@@ -15,9 +11,3 @@ Sample output:
     [4,5]
     [2,4]
     [4,3]
-
-creates a game board and a knight.
-Treat all possible moves the knight could make as children in a tree. 
-(stay on the board)
-Decide which search algorithm is best to use for this case.
-Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square.
--- index.js
+++ index.js
@@ -1,95 +1,71 @@
+const GridSize = 8;
+let shortestRoute = 0;
+
+function isOffGrid(row, col) {
+    return row < 0 || row > 7 || col < 0 || col > 7 ? true : false;
+}
+
 function shortestArray(a, b, c, d, e, f, g, h) {
-    if (!a && !b && !c && !d && !e && !f && !g && !h) {
-      return null;
-    }
+    if (!a && !b && !c && !d && !e && !f && !g && !h) { return null }
     let arr = [...arguments];
     let min = arr.reduce(function(prev, curr) {
-      if (!curr) { return prev }
-      if (!prev) { return curr }
-      return prev.length < curr.length ? prev : curr
+        if (!curr) { return prev }
+        if (!prev) { return curr }
+        return prev.length < curr.length ? prev : curr
     });
-    return arr[arr.indexOf(min)];
-  }
-  
-  let shortestRoute = 0;
-  
-  function branch(queue, start){
-    let route = queue.slice();
-    route.push(start);
-    return route > shortestRoute ? null : route;
-  }
-  
-  function trackMoves(start, end, queue) {
-    // Exit conditions
-    if (queue.length > shortestRoute) {
-      return null; // Current route is longer than shortestRoute
+    return arr[arr.indexOf(min)];           
+}
+
+function getRoutes(route, moves) {
+    let routes = [];
+    for (let i = 0; i < GridSize; i++) {
+        let temp = route.slice(); // Clone route
+        temp.push(moves[i]); // Add move to route
+        routes.push(temp); // Add route to routes
     }
-    if (start[0] === end[0] && start[1] === end[1]) { // End is found
-      if (queue.length <= shortestRoute) {
-        // Current route is less than/equal to shortest route
-        shortestRoute = queue.length;
-        return queue;
-      }
-      return null;
+    return routes;
+}
+
+function getMoves(row, col) {
+    return [
+        [row - 2, col + 1],
+        [row - 2, col - 1],
+        [row + 2, col + 1],
+        [row + 2, col - 1],
+        [row - 1, col + 2],
+        [row - 1, col - 2],
+        [row + 1, col + 2],
+        [row + 1, col - 2]
+    ];
+}
+
+function trackMoves(start, end, route) {
+    // If end is found
+    if (start[0] === end[0] && start[1] === end[1]) { return route }
+    // If current route is longer than shortest route
+    if (route.length > shortestRoute) { return null }
+    // If knight is off grid
+    if (isOffGrid(...start)) { return null }
+    // Get all moves for current position
+    let moves = getMoves(...start);
+    // Get all possible routes for current position
+    let routes = getRoutes(route.slice(), moves);
+
+    for (let i = 0; i < GridSize; i++) {
+        routes[i] = isOffGrid(...moves[i]) ? null : trackMoves(moves[i], end, routes[i])
     }
-    let isOffGrid = start[0] < 0 || start[0] > 7 || start[1] < 0 || start[1] > 7;
-    if (isOffGrid) {
-      return null; // Knight is off grid
-    }
-    let temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8;
-    let queue1 = queue.slice();
-    let queue2 = queue.slice();
-    let queue3 = queue.slice();
-    let queue4 = queue.slice();
-    let queue5 = queue.slice();
-    let queue6 = queue.slice();
-    let queue7 = queue.slice();
-    let queue8 = queue.slice();
-    temp1 = [start[0] - 2, start[1] + 1];
-    queue1.push(temp1);
-    queue1 = queue1.length < shortestRoute ? trackMoves(temp1, end, queue1) : null;
-    
-    temp2 = [start[0] - 2, start[1] - 1];
-    queue2.push(temp2);
-    queue2 = queue2.length < shortestRoute ? trackMoves(temp2, end, queue2) : null;
-    
-    temp3 = [start[0] + 2, start[1] + 1];
-    queue3.push(temp3);
-    queue3 = queue3.length < shortestRoute ? trackMoves(temp3, end, queue3) : null;
-    
-    temp4 = [start[0] + 2, start[1] - 1];
-    queue4.push(temp4);
-    queue4 = queue4.length < shortestRoute ? trackMoves(temp4, end, queue4) : null;
-    
-    temp5 = [start[0] - 1, start[1] + 2];
-    queue5.push(temp5);
-    queue5 = queue5.length < shortestRoute ? trackMoves(temp5, end, queue5) : null;
-    
-    temp6 = [start[0] - 1, start[1] - 2];
-    queue6.push(temp6);
-    queue6 = queue6.length < shortestRoute ? trackMoves(temp6, end, queue6) : null;
-    
-    temp7 = [start[0] + 1, start[1] + 2];
-    queue7.push(temp7);
-    queue7 = queue7.length < shortestRoute ? trackMoves(temp7, end, queue7) : null;
-    
-    temp8 = [start[0] + 1, start[1] - 2];
-    queue8.push(temp8);
-    queue8 = queue8.length < shortestRoute ? trackMoves(temp8, end, queue8) : null;
-  
-    return shortestArray(queue1, queue2, queue3, queue4, queue5, queue6, queue7, queue8);
-  }
-  
-  function knightTravails(a, b){
+    return shortestArray(...routes);
+}
+
+function knightMoves(a, b) {
     let path = null;
     while (path === null) {
-      path = trackMoves(a, b, []);
-      shortestRoute++;
+        path = trackMoves(a, b, []);
+        shortestRoute++;
     }
     console.log('You made it in ' + path.length + ' moves! Here\'s your path:')
     path.forEach(move => console.log(move));
     shortestRoute = 0;
-  }
-  
-  let path = knightTravails([0, 0], [7, 7]);
-  
+}
+
+let path = knightMoves([0, 0], [0, 0]);
