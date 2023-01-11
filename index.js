62908ec0e497084eebdd0a00aba7f079731041c6
Katherine de Vay <katdevay2@gmail.com>
2023-01-10 19:39:34 -0600

    Begin refactoring

--- index.js
+++ index.js
@@ -1,31 +1,95 @@
-function findNodeProps(row, col) {
-    let values = {
-        top: row - 1 < 0 ? null : [row - 1, col],
-        left: col - 1 < 0 ? null : [row, col - 1],
-        right: col + 1 > 7 ? null : [row, col + 1],
-        bottom: row + 1 > 7 ? null : [row + 1, col]
+function shortestArray(a, b, c, d, e, f, g, h) {
+    if (!a && !b && !c && !d && !e && !f && !g && !h) {
+      return null;
     }
-    return values;
-}
-
-class Node {
-    constructor(a, b) {
-        this.value = [a, b];
-        let siblings = findNodeProps(a, b);
-        this.top = siblings.top;
-        this.left = siblings.left;
-        this.right = siblings.right;
-        this.bottom = siblings.bottom;
+    let arr = [...arguments];
+    let min = arr.reduce(function(prev, curr) {
+      if (!curr) { return prev }
+      if (!prev) { return curr }
+      return prev.length < curr.length ? prev : curr
+    });
+    return arr[arr.indexOf(min)];
+  }
+  
+  let shortestRoute = 0;
+  
+  function branch(queue, start){
+    let route = queue.slice();
+    route.push(start);
+    return route > shortestRoute ? null : route;
+  }
+  
+  function trackMoves(start, end, queue) {
+    // Exit conditions
+    if (queue.length > shortestRoute) {
+      return null; // Current route is longer than shortestRoute
     }
-}
-
-const GameBoard  = {
-    constructor(gridSize) {
-        this.arr = [];
-        for(let i = 0; i < gridSize; i++) {
-            for(let j = 0; j < gridSize; j++) {
-                this.arr[i, j] = new Node(i, j);
-            }
-        }
+    if (start[0] === end[0] && start[1] === end[1]) { // End is found
+      if (queue.length <= shortestRoute) {
+        // Current route is less than/equal to shortest route
+        shortestRoute = queue.length;
+        return queue;
+      }
+      return null;
     }
-}
+    let isOffGrid = start[0] < 0 || start[0] > 7 || start[1] < 0 || start[1] > 7;
+    if (isOffGrid) {
+      return null; // Knight is off grid
+    }
+    let temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8;
+    let queue1 = queue.slice();
+    let queue2 = queue.slice();
+    let queue3 = queue.slice();
+    let queue4 = queue.slice();
+    let queue5 = queue.slice();
+    let queue6 = queue.slice();
+    let queue7 = queue.slice();
+    let queue8 = queue.slice();
+    temp1 = [start[0] - 2, start[1] + 1];
+    queue1.push(temp1);
+    queue1 = queue1.length < shortestRoute ? trackMoves(temp1, end, queue1) : null;
+    
+    temp2 = [start[0] - 2, start[1] - 1];
+    queue2.push(temp2);
+    queue2 = queue2.length < shortestRoute ? trackMoves(temp2, end, queue2) : null;
+    
+    temp3 = [start[0] + 2, start[1] + 1];
+    queue3.push(temp3);
+    queue3 = queue3.length < shortestRoute ? trackMoves(temp3, end, queue3) : null;
+    
+    temp4 = [start[0] + 2, start[1] - 1];
+    queue4.push(temp4);
+    queue4 = queue4.length < shortestRoute ? trackMoves(temp4, end, queue4) : null;
+    
+    temp5 = [start[0] - 1, start[1] + 2];
+    queue5.push(temp5);
+    queue5 = queue5.length < shortestRoute ? trackMoves(temp5, end, queue5) : null;
+    
+    temp6 = [start[0] - 1, start[1] - 2];
+    queue6.push(temp6);
+    queue6 = queue6.length < shortestRoute ? trackMoves(temp6, end, queue6) : null;
+    
+    temp7 = [start[0] + 1, start[1] + 2];
+    queue7.push(temp7);
+    queue7 = queue7.length < shortestRoute ? trackMoves(temp7, end, queue7) : null;
+    
+    temp8 = [start[0] + 1, start[1] - 2];
+    queue8.push(temp8);
+    queue8 = queue8.length < shortestRoute ? trackMoves(temp8, end, queue8) : null;
+  
+    return shortestArray(queue1, queue2, queue3, queue4, queue5, queue6, queue7, queue8);
+  }
+  
+  function knightTravails(a, b){
+    let path = null;
+    while (path === null) {
+      path = trackMoves(a, b, []);
+      shortestRoute++;
+    }
+    console.log('You made it in ' + path.length + ' moves! Here\'s your path:')
+    path.forEach(move => console.log(move));
+    shortestRoute = 0;
+  }
+  
+  let path = knightTravails([0, 0], [7, 7]);
+  
